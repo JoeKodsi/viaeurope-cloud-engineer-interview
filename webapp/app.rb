5e69934fc9aaf045d39fdecbc72e7742c34b8242
Richard Marbach <rmarbach@gmail.com>
2022-06-28 08:23:36 +0200

    Add `/sum` endpoint
    This endpoint let's us simulate various scenarios:
    
    n == 13: Application shuts down. Needs to be restarted to recover
    n > 99999999: Application will use 100% CPU for a long time

--- webapp/app.rb
+++ webapp/app.rb
@@ -5,3 +5,36 @@ get "/" do
 
   {greeting: "Hello world."}.to_json
 end
+
+post "/sum" do
+  content_type :json
+
+  request.body.rewind
+  data = JSON.parse(request.body.read)
+
+  n = data["n"]
+
+  if n.nil?
+    halt 422, {error: "n needs to be set"}.to_json
+  end
+
+  unless n.is_a?(Integer)
+    halt 422, {error: "n needs to be an integer"}.to_json
+  end
+
+  if n < 1
+    halt 422, {error: "n needs to be greater 0"}.to_json
+  end
+
+  if n == 13
+    Sinatra::Application.quit! # Crash and burn
+  end
+
+  {sum: slow_sum(n)}.to_json
+rescue
+  halt 400, {error: "invalid json"}.to_json
+end
+
+def slow_sum(n)
+  (0..n).inject(0, &:+)
+end
